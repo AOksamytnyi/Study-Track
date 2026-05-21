a457208a29883159ce06a443a33b04de4c41f8de
AOksamytnyi <285827882+AOksamytnyi@users.noreply.github.com>
2026-05-22 02:44:35 +0300

    feat(database): add initial prisma models

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "prisma/config";
 import process from process
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: "prisma/schema",
   migrations: {
     path: "prisma/migrations",
   },

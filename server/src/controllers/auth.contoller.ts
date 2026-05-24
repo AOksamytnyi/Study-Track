b2c213e938b559dcacae87d78881ce03c51a8bdf
AOksamytnyi <285827882+AOksamytnyi@users.noreply.github.com>
2026-05-24 17:08:00 +0300

    feat(auth): generate jwt token on register

--- server/src/controllers/auth.contoller.ts
+++ server/src/controllers/auth.contoller.ts
@@ -1,7 +1,8 @@
 import prisma from "../lib/prisma";
-import { Request, Response } from 'express'
+import { Request, Response } from "express";
 
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 export async function register(req: Request, res: Response) {
   const { email, password, username } = req.body;
@@ -10,19 +11,25 @@ export async function register(req: Request, res: Response) {
     return res.status(400).json({ message: "Email and password are required" });
   }
 
-  const existing = await prisma.user.findUnique({where: {email}})
-  if(existing){
-    return res.status(409).json({message: "User already exists"})
+  if (!process.env.JWT_SECRET) {
+    return res.status(500).json({ message: "JWT secret is not configured" });
   }
-  
-  const passwordHash = await bcrypt.hash(password, 10)
 
-  console.log(passwordHash)
+  const existing = await prisma.user.findUnique({ where: { email } });
+  if (existing) {
+    return res.status(409).json({ message: "User already exists" });
+  }
+
+  const passwordHash = await bcrypt.hash(password, 10);
+
 
   const user = await prisma.user.create({
-    data: {email, passwordHash, username}
-  })
+    data: { email, passwordHash, username },
+  });
+  
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET!, {
+    expiresIn: "7d",
+  });
 
-  return res.status(201).json({ user })
-
+  return res.status(201).json({ token });
 }

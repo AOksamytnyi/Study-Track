3972c7e580e79fd6d642bfdd6d495ce5a6644f08
AOksamytnyi <285827882+AOksamytnyi@users.noreply.github.com>
2026-05-28 00:16:19 +0300

    feat(auth): add jwt auth middleware

---
+++ server/src/middleware/auth.middleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express'
+
+const jwt = require("jsonwebtoken");
+
+export function authMiddleware(req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization 
+
+  if(!authHeader || !authHeader.startsWith("Bearer ")){
+    return res.status(401).json({ message: 'No token provided' })
+  }
+
+  const token = authHeader.split(' ')[1]
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET) as { userId: string }
+    req.userId = payload.userId
+    next()
+
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid token' })
+  }
+}

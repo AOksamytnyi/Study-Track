6ce768439cf02f2e58ac784a35687bbb0148f19f
AOksamytnyi <285827882+AOksamytnyi@users.noreply.github.com>
2026-05-27 23:58:03 +0300

    feat(auth): add login endpoint

--- server/src/controllers/auth.contoller.ts
+++ server/src/controllers/auth.contoller.ts
@@ -22,14 +22,41 @@ export async function register(req: Request, res: Response) {
 
   const passwordHash = await bcrypt.hash(password, 10);
 
-
   const user = await prisma.user.create({
     data: { email, passwordHash, username },
   });
-  
+
   const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET!, {
     expiresIn: "7d",
   });
 
   return res.status(201).json({ token });
+}
+
+export async function login(req: Request, res: Response) {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are empty" });
+  }
+
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (!user) {
+    return res.status(401).json({ message: "Email or password are incorrect" });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.passwordHash);
+
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: "Email or password are incorrect" });
+  }
+
+  const token = jwt.sign(
+  { userId: user.id },
+  process.env.JWT_SECRET!,
+  { expiresIn: '7d' }
+)
+
+  return res.status(200).json({ token });
 }
--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { asyncHandler } from "../utils/async-handler";
-import { register } from "../controllers/auth.contoller";
+import { login, register } from "../controllers/auth.contoller";
 
 export const authRouter = Router()
 
 authRouter.post('/register', asyncHandler(register))
+authRouter.post('/login', asyncHandler(login))

ee2cb005ae5cfcca2b79cded979da682a02241fa
AOksamytnyi <285827882+AOksamytnyi@users.noreply.github.com>
2026-06-03 18:15:29 +0300

    feat(auth): persist auth token in localstorage

--- client/src/store/authStore.tsx
+++ client/src/store/authStore.tsx
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type User = {
   id: number;
@@ -18,9 +19,16 @@ interface IAuthActions {
 
 interface IAuthStore extends IAuthActions, IAuthState {}
 
-export const useAuthStore = create<IAuthStore>()((set) => ({
-  token: null,
-  user: null,
-  login: (token, user) => set({ token, user }),
-  logout: () => set({ token: null, user: null }),
-}));
+export const useAuthStore = create<IAuthStore>()(
+  persist(
+    (set) => ({
+      token: null,
+      user: null,
+      login: (token, user) => set({ token, user }),
+      logout: () => set({ token: null, user: null }),
+    }),
+    {
+      name: "auth",
+    },
+  ),
+);

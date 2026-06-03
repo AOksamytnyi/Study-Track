ca99661b54c794cc0d5c03c232e3fc9bfb900437
AOksamytnyi <285827882+AOksamytnyi@users.noreply.github.com>
2026-06-03 18:46:33 +0300

    feat(auth): add route guards for persisted sessions

--- client/src/store/authStore.tsx
+++ client/src/store/authStore.tsx
@@ -10,11 +10,13 @@ type User = {
 interface IAuthState {
   token: string | null;
   user: User | null;
+  hasHydrated: boolean;
 }
 
 interface IAuthActions {
   login: (token: string, user: User) => void;
   logout: () => void;
+  setHasHydrated: (value: boolean) => void;
 }
 
 interface IAuthStore extends IAuthActions, IAuthState {}
@@ -24,11 +26,16 @@ export const useAuthStore = create<IAuthStore>()(
     (set) => ({
       token: null,
       user: null,
+      hasHydrated: false,
       login: (token, user) => set({ token, user }),
       logout: () => set({ token: null, user: null }),
+      setHasHydrated: (value) => set({ hasHydrated: value }),
     }),
     {
       name: "auth",
+      onRehydrateStorage: () => (state) => {
+        state?.setHasHydrated(true);
+      },
     },
   ),
 );
